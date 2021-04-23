d578d0c3289c050d00ee4a045f5325f6432525f4
Emily Skerl <emilyskerl@gmail.com>
2021-04-23 15:46:49 -0400

    Added dog class and pseudodata

---
+++ DogDatabase/Models/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace DogDatabase.Models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Birthday { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Dog>(this);
+    }
+}
