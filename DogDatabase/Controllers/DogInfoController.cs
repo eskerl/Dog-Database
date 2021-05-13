55363b4589572d1646ddde6df4e4e76a5342607a
Emily Skerl <emilyskerl@gmail.com>
2021-05-12 21:21:54 -0400

    Added new 'Dogs' view and updated layout

---
+++ DogDatabase/Controllers/DogInfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogDatabase.Controllers
+{
+    public class DogInfoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DogDatabase/Controllers/HomeController.cs
+++ DogDatabase/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ namespace DogDatabase.Controllers
             return View();
         }
 
+        public IActionResult Dog()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
