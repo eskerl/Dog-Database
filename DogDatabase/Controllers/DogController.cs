3677c58a53237f14af990654d8227a246bbcbc94
eskerl <emilyskerl@gmail.com>
2021-05-18 22:10:13 -0400

    Renamed DogInfo to Dog, Added sample rendered dog page

---
+++ DogDatabase/Controllers/DogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DogDatabase.Models;
+
+namespace DogDatabase.Controllers
+{
+    public class DogController : Controller
+    {
+        // GET: /Dog/
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            Dog dog = new Dog() { Name = "Sammy" };
+            return View(dog);
+        }
+    }
+}
