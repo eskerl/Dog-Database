c140c5e61f7313a6df6b9b97e05b5bf628cc7a24
eskerl <emilyskerl@gmail.com>
2021-05-21 12:12:23 -0400

    Adjust View Text

--- DogWebApp/Models/Dog.cs
+++ DogWebApp/Models/Dog.cs
@@ -7,14 +7,20 @@ namespace DogWebApp.Models
     public class Dog
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public string Breed { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d MMM yyyy}")]
         public DateTime? Birthday { get; set; }
+
 
         [Range(0, 30)]
         public short? Age { get; set; }
+
 
         [Range(0.0, 350.0)]
         public float? Weight { get; set; }
--- DogWebApp/Models/Owner.cs
+++ DogWebApp/Models/Owner.cs
@@ -9,15 +9,23 @@ namespace DogWebApp.Models
     public class Owner
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(80)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
         [Required]
         [StringLength(80)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         public string Address { get; set; }
+
         [StringLength(16)]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
         public List<Dog> Dogs { get; set; }
     }
 }
